25c3a4d6b95551a0b18b755350041d3efe9909d8
Foisems <foisems@mail.ru>
2023-01-22 17:55:26 +0300

    Изменены цвет формы, в обработчик конпки выхода добавлен запрос на выход

--- WindowsFormsAppTestGit/Form1.Designer.cs
+++ WindowsFormsAppTestGit/Form1.Designer.cs
@@ -64,6 +64,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
             this.ClientSize = new System.Drawing.Size(372, 159);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
--- WindowsFormsAppTestGit/Form1.cs
+++ WindowsFormsAppTestGit/Form1.cs
@@ -19,12 +19,12 @@ namespace WindowsFormsAppTestGit
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+           
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
     }
 }
